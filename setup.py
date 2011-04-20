eba6727c5b8ded97844cc83f9d34a86a0e71b2df
scastillo <sebastian.castillo@cenuage.com>
2011-04-20 11:04:08 -0500

    Added setup stuff

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from siesta import __version__
+
+fd = open("README.md")
+long_description = fd.read()
+fd.close()
+
+setup(
+    name='siesta',
+    version=__version__,
+    description='Sit back, relax and enjoy of your python REST client',
+    author='Sebastián Castillo Builes',
+    author_email='castillobuiles@gmail.com',
+    license='BSD',
+    keywords='REST RESTful API HTTP web service',
+    url="",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires = ['simplejson'],
+    classifiers=[
+        "Development Status :: 4 - Alpha",
+        "License :: OSI Approved :: GPL License",
+        ],
+    )
