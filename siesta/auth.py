3a099d94bac801c5699f7214801ed0be502f2eab
Sebastian Castillo Builes <castillobuiles@gmail.com>
2011-05-21 10:45:22 -0500

    Basic auth

--- siesta/auth.py
+++ siesta/auth.py
@@ -23,3 +23,22 @@ class APIKeyAuth(Auth):
 
     def make_headers(self):
         return {self.auth_header_name: self.api_key, }
+
+Class BasicAuth(Auth):
+    """
+    Authentication using username and password
+    """
+
+    def __init__(self, api_username, api_password, auth_header_name="X-Authorization"):
+        self.api_username = api_username
+        self.api_password = api_password
+        self.auth_header_name = auth_header_name
+        
+    def encode_params(self):
+        basic_token = base64.encodestring('' + str(self.api_username) + ':' + str(self.api_password))
+        basic_token = basic_token.replace('\n', '')
+        return basic_token
+    
+    def make_headers(self):
+        token = self.encode_params()
+return {self.auth_header_name: 'Basic ' + token, }
